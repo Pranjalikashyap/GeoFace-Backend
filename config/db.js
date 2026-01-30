70e5d3fa199ad1dd382cbbda38e611c931ad4017
pranjali kashyap <pranjalikashyap@MacBook-Air-M1.local>
2026-01-30 13:20:20 +0530

    Clean production-ready server for Render

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
+const Mongo_Url = process.env.MONGO_URI; // don't use localhost fallback for deployment
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(Mongo_Url);
         console.log("✅ Connected to MongoDB Atlas");
     } catch (err) {
-        console.error("❌ MongoDB connection error:", err.message);
+        console.log("❌ MongoDB connection error:", err);
         process.exit(1);
     }
 };
 
 module.exports = connectDB;
-
